d7f2c24ae043e09f3ff689ba00dce0980b996b74
said_sadu <sadullaev.1996@gmail.com>
2022-07-09 18:12:41 +0300

    третья задача решена

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0) Console.WriteLine("Четное");  //УСЛОВИЕ: введенное число делим на 2 и проверяем, остается ли остаток, если нет, то число четное, если да, то число нечетное
+else Console.WriteLine("Нечетное");
+Console.ReadKey();
