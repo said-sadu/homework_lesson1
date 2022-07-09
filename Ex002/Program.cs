766b5592e51ee1446210f194df2c67a45cada359
said_sadu <sadullaev.1996@gmail.com>
2022-07-09 18:48:57 +0300

    изменение 4 задачи

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -6,22 +6,3 @@ int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
-
-if (a > b)
-{
-    int (max = a);
-}
-else if (a < b)
-{
-    int (max = b);
-}
-if (max > c)
-{
-    Console.WriteLine("Большее число");
-    Console.WriteLine(max);
-}
-else (max < c);
-{
-    Console.WriteLine("Большее число");
-    Console.WriteLine(max);
-}
